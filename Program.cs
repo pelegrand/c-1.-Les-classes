6c068f817490aeca377273a83b203d356e30f968
Paul-Emmanuel Legrand <paulm.legrand@gmail.com>
2022-03-04 14:45:06 +0100

    les conditions

--- Program.cs
+++ Program.cs
@@ -1,76 +1,40 @@
 ﻿
 using System;
 
-Random rnd = new Random();
+Conditions game = new Conditions();
 
-Character char1 = new Character();
-char1.attackStrength = rnd.Next(1, 100);
-char1.lifePoints = rnd.Next(1, 100);
-char1.deffenceStrength = rnd.Next(1, 100);
-
-
-Character char2 = new Character();
-char2.lifePoints = rnd.Next(1, 100);
-char2.attackStrength = rnd.Next(1, 100);
-char2.deffenceStrength = rnd.Next(1, 100);
-
-char1.getNames(char2);
-char1.IsAlive();
-char2.IsAlive();
-
-while (char1.lifePoints > 0 && char2.lifePoints > 0)
+game.test();
+public class Conditions
 {
-  char1.Attack(char2);
-  char1.IsAlive();
-  char2.IsAlive();
-};
-
-public class Character
-{
-  public string name;
-  public int lifePoints;
-  public int attackStrength;
-  public int deffenceStrength;
-  
-  
-  public void getNames(Character attacker)
+  public void test()
   {
-    Console.WriteLine("Enter username:");
+    Console.WriteLine("Enter minimum number : ");
 
-    name = Console.ReadLine();
+    int.TryParse(Console.ReadLine(), out int min);
 
-    Console.WriteLine("Enter Attacker Name:");
+    Console.WriteLine("Enter a maximum number : ");
 
-    attacker.name = Console.ReadLine();
+    int.TryParse(Console.ReadLine(), out int max);
+
+    Console.WriteLine("Enter a number include between to two firsts :");
+
+    int.TryParse(Console.ReadLine(), out int last);
+
+    while(last<min || last>max){
+     if(last < min){
+        Console.WriteLine("It must be superior to " + min + " Try again");
+        Console.WriteLine("Enter a third number include between to two firsts :");
+        int.TryParse(Console.ReadLine(), out last);
+      }else{
+        Console.WriteLine("It must be inforior to " + max);
+        Console.WriteLine("Enter a third number include between to two firsts :");
+        int.TryParse(Console.ReadLine(), out last);
+      }
+    }
+
+    Console.WriteLine("it's ok " + last + " is include between " + min + " and " + max);
 
   }
-  public void IsAlive()
-  {
-    if (lifePoints > 0)
-    {
-      Console.WriteLine(name + " is Alive, life-points :" + lifePoints);
-    }
-    else
-    {
-      Console.WriteLine(name + " is dead ");
-    }
-  }
-
-  public void Attack(Character attacker)
-  {
-    Console.WriteLine(attacker.name + " Attack " + name + " With " + attacker.attackStrength + " Strength");
-    if (attacker.attackStrength < deffenceStrength)
-    {
-      Console.WriteLine("But " + name + " is strong " + " and defend himself " );
-      attacker.lifePoints = (attacker.lifePoints) - (attackStrength - attacker.deffenceStrength);
-    }
-    else
-    {
-      Console.WriteLine(name + " loose " + (attacker.attackStrength - deffenceStrength) + " lifepoints ");
-      lifePoints = (lifePoints) - (attacker.attackStrength - deffenceStrength);
-    }
-  }
-
 
 }
 
